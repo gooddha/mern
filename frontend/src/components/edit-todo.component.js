60c26e1ebf1461f3931fdb9d72323a4ea1cd8d80
gooddha <gooddha@ya.ru>
2019-11-30 17:09:14 +0300

    done the whole tutorial

--- frontend/src/components/edit-todo.component.js
+++ frontend/src/components/edit-todo.component.js
@@ -6,6 +6,9 @@ export default class EditTodo extends Component {
   constructor(props) {
     super(props);
 
+    this.onChange = this.onChange.bind(this);    
+    this.onSubmit = this.onSubmit.bind(this);
+
     this.state = {
       todo_description: '',
       todo_responsible: '',
@@ -13,6 +16,7 @@ export default class EditTodo extends Component {
       todo_completed: false
     }
   }
+
 
   componentDidMount() {
     axios.get('http://localhost:4000/todos/' + this.props.match.params.id)
@@ -26,6 +30,39 @@ export default class EditTodo extends Component {
       }).catch(function(err) {
         console.log(err);
       })
+  }
+
+  onChange(e) {
+    if (e.target.id === 'completedCheckbox') {
+      this.setState({
+        todo_completed: !this.state.todo_completed
+      });
+      return
+    }
+
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+    console.log(this.state);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const obj ={
+      todo_description: this.state.todo_description,
+      todo_responsible: this.state.todo_responsible,
+      todo_priority: this.state.todo_priority,
+      todo_completed: this.state.todo_completed
+    };
+
+    axios.post('http://localhost:4000/todos/update/'+this.props.match.params.id, obj)
+    .then(res => {
+      console.log(res.data)
+      this.props.history.push('/');
+    });
+
+
   }
 
   render() {
@@ -52,13 +89,51 @@ export default class EditTodo extends Component {
                     />
           </div>
           <div className="form-group">
-            <label>Piority:</label>
-            <input type="text"
-                    className="form-control"
-                    value={this.state.todo_responsible}
-                    name = "todo_responsible"
-                    onChange={this.onChange}
-                    />
+            <div className="form-check form-check-inline">
+              <input className="form-check-input"
+                     type="radio"
+                     name = "todo_priority"
+                     value="Low"
+                     checked={this.state.todo_priority==='Low'}
+                     onChange={this.onChange}
+                     />
+              <label className="form-check-label">Low</label>
+            </div>            
+            <div className="form-check form-check-inline">
+              <input className="form-check-input"
+                     type="radio"
+                     name = "todo_priority"
+                     value="Medium"
+                     checked={this.state.todo_priority==='Medium'}
+                     onChange={this.onChange}
+                     />
+              <label className="form-check-label">Medium</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input className="form-check-input"
+                     type="radio"
+                     name="todo_priority"
+                     value="High"
+                     checked={this.state.todo_priority==='High'}
+                     onChange={this.onChange}
+                     />
+              <label className="form-check-label">High</label>
+            </div>
+            <div className="form-check">
+              <input  type="checkbox"
+                      className="form-check-input"
+                      id="completedCheckbox"
+                      name="todo_completed"
+                      onChange={this.onChange}
+                      checked={this.state.todo_completed}
+                      value={this.state.todo_completed}
+                      />
+              <label className="form-check-label" htmlFor="completedCheckbox">Completed</label>
+            </div>
+            <br />
+            <div className="form-group">
+              <input type="submit" value="Update todo" className="btn btn-primary" />
+            </div>  
           </div>
         </form>
       </div>
